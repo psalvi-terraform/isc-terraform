bc28219e6e56f94644f9209df91a8268c749eac3
prathmesh salvi <prathmeshsalvi111@gmail.com>
2022-01-17 00:13:49 +0530

    added subnet

--- main.tf
+++ main.tf
@@ -28,3 +28,18 @@ resource "azurerm_virtual_network" "vnet1" {
   location            = azurerm_resource_group.demorg.location
   address_space       = ["10.0.0.0/16"]
 }
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
